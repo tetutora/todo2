4598d68dd2f752b589bb59bbd0cf5cacfcefa70a
kitano <beiyelinai01@gmail.com>
2024-11-18 22:00:18 +0900

    更新

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts/app')
+@extends('layouts.app')
 
 @section('css')
 <link rel="stylesheet" href="{{asset('index.css')}}">
@@ -8,26 +8,26 @@
 
 <div class="todo-form__content">
     <div class="todo-form__heading">
-        <h2>Todoを作成しました</h2>
+        <p>Todoを作成しました</p>
     </div>
     <form action="/index" class="form" method="post">
         @csrf
         <input type="text" name="text" id="">
-        <input type="button" value="">
         <button class="form__button-submit" type="submit">作成</button>
     </form>
     <table class="todo-table">
         <tr class="todo-table__title">Todo</tr>
         <tr>
             <th>test1</th>
-            <td><input type="button" value="">更新</td>
-            <td><input type="button" value="">削除</td>
+            <td><input type="button" value="更新"></td>
+            <td><input type="button" value="削除"></td>
         </tr>
         <tr>
-            <th>test1</th>
-            <td><input type="button" value="">更新</td>
-            <td><input type="button" value="">削除</td>
+            <th>test2</th>
+            <td><input type="button" value="更新"></td>
+            <td><input type="button" value="削除"></td>
         </tr>
     </table>
 
 </div>
+@endsection
