6d4b94a15727126ab7a44f6b2c28cb3fe5d27211
kitano <beiyelinai01@gmail.com>
2024-11-19 15:38:07 +0900

    更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,6 @@ use App\Http\Controllers\TodoController;
 // });
 
 Route::get('/',[TodoController::class,'index']);
+Route::post('/todos',[TodoController::class,'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::post('/todos/delete',[TodoController::class,'destroy']);
